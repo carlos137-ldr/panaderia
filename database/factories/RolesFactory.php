3825ff0b1567f8a286891702c0fd52f8345b8f62
Juan Carlos GV <s22030125@itsch.edu.mx>
2025-10-17 22:47:07 -0600

    nuevamente

--- database/factories/RolesFactory.php
+++ database/factories/RolesFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class RoleFactory extends Factory
+class RolesFactory extends Factory
 {
     public function definition(): array
     {
